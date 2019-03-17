d854354e8777aea9209d21b8d16dca2e9f7b472e
tianwei40 <Wstw180!@#$%!>
2019-03-17 23:11:06 +0800

    sss

--- src/main/java/com/code/algorithm/leetcode/MaxSubArray.java
+++ src/main/java/com/code/algorithm/leetcode/MaxSubArray.java
@@ -12,28 +12,27 @@ public class MaxSubArray {
 
     /**
      * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
-     *
+     * <p>
      * 示例: 输入: [-2,1,-3,4,-1,2,1,-5,4], 输出: 6
-     *
+     * <p>
      * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。 进阶:
-     * 
+     * <p>
      * 如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
-     * 
+     *
      * @param nums
      * @return
      */
     public static int maxSubArray(int[] nums) {
-        int result = nums[0];
+        int result = 0;
         int sum = nums[0];
-        int length = nums.length;
-        for (int i = 1; i < length; i++) {
-            int num = nums[i];
-            if (sum > 0) {
-                sum += num;
-            } else {
+        for (int num : nums) {
+            //如果小于0就不加了，加了就小了
+            if (sum < 0) {
                 sum = num;
+            } else {
+                sum = sum + num;
             }
-            result = Math.max(sum, result);
+            result = Math.max(result, sum);
         }
         return result;
     }
--- src/main/java/com/code/jdk/thead/SynchronizedDemo.java
+++ src/main/java/com/code/jdk/thead/SynchronizedDemo.java
@@ -12,9 +12,68 @@ public class SynchronizedDemo {
     private static AtomicInteger sum = new AtomicInteger(1);
     private static Object lock = new Object();
 
-    public static void main(String[] args) {
-        // synchronized
-        sync();
+    public static void main(String[] args) throws InterruptedException {
+        //交替打印123
+        //sync();
+        //死锁
+        //dealLock();
+        // 线程优先级
+        //join();
+        //中断线程
+        //interrupt();
+    }
+
+    private static void interrupt() throws InterruptedException {
+        Thread t1 = new Thread() {
+            @Override public void run() {
+                while (true) {
+                    if (Thread.currentThread().isInterrupted()) {
+                        System.out.println(Thread.currentThread().getName() + "----线程已中断");
+                        break;
+                    }
+                    System.out.println(Thread.currentThread().getName() + "----" + sum.incrementAndGet());
+                }
+            }
+        };
+        t1.start();
+        Thread.sleep(5);
+        t1.interrupt();
+    }
+
+    private static void dealLock() {
+        Object l1 = new Object();
+        Object l2 = new Object();
+        Thread t1 = new Thread() {
+            @Override public void run() {
+                synchronized (l1) {
+                    try {
+                        Thread.sleep(50);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    synchronized (l2) {
+                    }
+                }
+            }
+        };
+        Thread t2 = new Thread() {
+            @Override public void run() {
+                synchronized (l2) {
+                    try {
+                        Thread.sleep(50);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    synchronized (l1) {
+
+                    }
+                }
+            }
+        };
+        t1.setName("t1");
+        t1.start();
+        t2.setName("t1");
+        t2.start();
     }
 
     /**
@@ -77,22 +136,30 @@ public class SynchronizedDemo {
         t3.start();
     }
 
-    public static void join() {
+    public static void join() throws InterruptedException {
         Thread t1 = new Thread(() -> {
             while (sum.get() < 100) {
-                System.out.println(sum.incrementAndGet());
-
+                System.out.println(Thread.currentThread().getName() + "----" + sum.incrementAndGet());
             }
         });
         Thread t2 = new Thread(() -> {
             while (sum.get() < 100) {
-                System.out.println(sum.incrementAndGet());
+                System.out.println(Thread.currentThread().getName() + "----" + sum.incrementAndGet());
+            }
+
+        });
+        Thread t3 = new Thread(() -> {
+            while (sum.get() < 100) {
+                System.out.println(Thread.currentThread().getName() + "----" + sum.incrementAndGet());
             }
         });
 
         t1.setName("t1");
         t2.setName("t2");
+        t3.setName("t3");
         t1.start();
         t2.start();
+        //        t3.start();
+        t2.join();
     }
 }
