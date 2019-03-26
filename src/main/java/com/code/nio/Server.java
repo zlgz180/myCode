99a80aba81d1d100ee26fdb0ad1caea66d5627bf
tianwei40 <tianwei8@jd.com>
2019-03-26 09:48:28 +0800

    ss

---
+++ src/main/java/com/code/nio/Server.java
@@ -0,0 +1,62 @@
+package com.code.nio;
+
+
+import com.google.common.primitives.Ints;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
+
+public class Server {
+
+    public static void main(String[] args) throws IOException {
+
+        // 实例化，并监听端口
+        AsynchronousServerSocketChannel server = AsynchronousServerSocketChannel.open()
+                .bind(new InetSocketAddress(8080));
+
+        // 自己定义一个 Attachment 类，用于传递一些信息
+        Attachment att = new Attachment();
+        att.setServer(server);
+
+        server.accept(att, new CompletionHandler<AsynchronousSocketChannel, Attachment>() {
+            @Override
+            public void completed(AsynchronousSocketChannel client, Attachment att) {
+                try {
+                    SocketAddress clientAddr = client.getRemoteAddress();
+                    System.out.println("收到新的连接：" + clientAddr);
+
+                    // 收到新的连接后，server 应该重新调用 accept 方法等待新的连接进来
+                    att.getServer().accept(att, this);
+
+                    Attachment newAtt = new Attachment();
+                    newAtt.setServer(server);
+                    newAtt.setClient(client);
+                    newAtt.setReadMode(true);
+                    newAtt.setBuffer(ByteBuffer.allocate(2048));
+
+                    // 这里也可以继续使用匿名实现类，不过代码不好看，所以这里专门定义一个类
+                    client.read(newAtt.getBuffer(), newAtt, new ChannelHandler());
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+            @Override
+            public void failed(Throwable t, Attachment att) {
+
+                Ints.toArray()
+                System.out.println("accept failed");
+            }
+        });
+        // 为了防止 main 线程退出
+        try {
+            Thread.currentThread().join();
+        } catch (InterruptedException e) {
+        }
+    }
+}
