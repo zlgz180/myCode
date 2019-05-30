54efa74cd3fd15c819a86eaca827fd11becf4219
tianwei40 <tianwei8@jd.com>
2019-05-30 09:59:49 +0800

    zzz

---
+++ src/main/java/com/code/algorithm/leetcode/FourSum.java
@@ -0,0 +1,53 @@
+package com.code.algorithm.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author tianwei
+ */
+public class FourSum {
+    public static void main(String[] args) {
+        int[] nums = { 1, 0, -1, 0, -2, 2, 3, -4, 5, 8 };
+        System.out.println(fourSum(nums, 0));
+    }
+
+    /**
+     * 给定一个包含 n 个整数的数组 nums 和一个目标值 target，判断 nums 中是否存在四个元素 a，b，c 和 d ，使得 a + b + c
+     * + d 的值与 target 相等？找出所有满足条件且不重复的四元组。
+     * <p>
+     * 注意：
+     * <p>
+     * 答案中不可以包含重复的四元组。
+     * <p>
+     * 示例：
+     * <p>
+     * 给定数组 nums = [1, 0, -1, 0, -2, 2]，和 target = 0。
+     * <p>
+     * 满足要求的四元组集合为： [ [-1, 0, 0, 1], [-2, -1, 1, 2], [-2, 0, 0, 2] ]
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    for (int l = k + 1; l < nums.length; l++) {
+                        if ((nums[i] + nums[j] + nums[k] == target - nums[l])) {
+                            List<Integer> list = new ArrayList<>();
+                            list.add(nums[i]);
+                            list.add(nums[j]);
+                            list.add(nums[k]);
+                            list.add(nums[l]);
+                            result.add(list);
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
